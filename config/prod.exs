4720293179798bc5c9ec13482caa4195a54e6bea
Abdullah Esmail <abdullah.esmail@gmail.com>
2020-05-13 01:26:05 +0300

    set database connection.

--- config/prod.exs
+++ config/prod.exs
@@ -50,4 +50,10 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+
+config :bakery, Bakery.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"},
+  database: "",
+  ssl: true,
+  pool_size: 2
