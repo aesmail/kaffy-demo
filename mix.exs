ed7a312709abceec27f5d63e0443fbd4bd7377f0
Abdullah Esmail <aesmail@hey.com>
2022-05-30 08:28:02 +0300

    update deps.

--- mix.exs
+++ mix.exs
@@ -45,8 +45,8 @@ defmodule Bakery.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      # {:kaffy, "~> 0.9.0"}
-      {:kaffy, path: "../kaffy"}
+      {:kaffy, github: "aesmail/kaffy"}
+      # {:kaffy, path: "../kaffy"}
     ]
   end
 
