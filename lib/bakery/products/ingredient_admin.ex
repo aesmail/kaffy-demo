7a31725cf73e89aed7ed18791b7277b9eff48274
Abdullah Esmail <aesmail@hey.com>
2022-10-31 14:56:20 +0300

    add a few widgets to the dashboard.

--- lib/bakery/products/ingredient_admin.ex
+++ lib/bakery/products/ingredient_admin.ex
@@ -1,25 +1,46 @@
 defmodule Bakery.Products.IngredientAdmin do
   def widgets(_, _) do
-    demo_content =
+    version_content =
       ~s(This is a demo of <a href="https://github.com/aesmail/kaffy">kaffy</a>, the elixir admin package for phoenix applications. Add a product with the word "fake" in the title and it will be deleted automatically within 60 seconds. Check the scheduled tasks feature.)
+
+    whats_new_content =
+      ~s(Fixed saving enum fields with their cast types. Check the <a href="https://github.com/aesmail/kaffy/blob/master/CHANGELOG.md">CHANGELOG</a> for more details.)
 
     [
       %{
         type: "text",
-        title: "Kaffy v0.9.3",
+        title: "Kaffy v0.9.4",
         order: 5,
-        width: 12,
-        content: {:safe, demo_content}
+        width: 6,
+        content: {:safe, version_content}
+      },
+      %{
+        type: "text",
+        title: "What's New",
+        order: 5,
+        width: 6,
+        content: {:safe, whats_new_content}
       },
       %{
         type: "chart",
-        title: "This week's sales",
+        title: "This week's imaginary sales",
         order: 10,
-        width: 12,
+        width: 6,
         content: %{
-          x: ["Mon", "Tue", "Wed", "Thu", "Today"],
-          y: [150, 230, 75, 240, 290],
+          x: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          y: [150, 230, 75, 240, 290, 300, 190],
           y_title: "USD"
+        }
+      },
+      %{
+        type: "chart",
+        title: "Imaginary visits (last week)",
+        order: 10,
+        width: 6,
+        content: %{
+          x: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          y: [500, 600, 480, 738, 800, 770, 920],
+          y_title: "Visits"
         }
       }
     ]
